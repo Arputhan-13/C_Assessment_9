ae8e720e04a6bace83f7f3703372ad142ad334fb
Arputhan-13 <karputhan@gmail.com>
2026-06-13 22:26:34 +0530

    Create C_problem_3.c

---
+++ C_problem_3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int a[5];
+    int small,i;
+    printf("Enter the 5 number : ");
+    for(i=0;i<5;i++)
+    {
+    scanf("%d",&a[i]);
+    }
+    small=a[0];
+    for(i=1;i<5;i++)
+    {
+    if(a[i]<small)
+    {
+    small = a[i];
+    }
+    }
+    printf("Smallest= %d",small);
+}
