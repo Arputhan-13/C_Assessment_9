bd7b8ee12355e0785aaef84d463da311a2ed6ac2
Arputhan-13 <karputhan@gmail.com>
2026-06-14 20:54:17 +0530

    Create C_problem_9.c

---
+++ C_problem_9.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int a[5],i;
+    printf("Enter the Number : ");
+    for(i=0;i<5;i++)
+    {
+    scanf("%d",&a[i]);
+    }
+    printf("\nThe Same Number : ");
+    for(i=0;i<5;i++)
+    {
+    printf("%d",a[i]);
+    }
+    printf("\nAfter Removing Even Number : ");
+    for(i=0;i<5;i++)
+    {
+    if(a[i]%2==1)
+    {
+    printf("%d",a[i]);
+    }
+    }
+}    
